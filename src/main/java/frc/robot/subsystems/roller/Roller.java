81d088bef65dbdb88a17a755dc2842c8aa4c3afd
akris011 <kris011@stu.gulliverprep.org>
2023-09-14 17:40:08 -0400

    structurish

---
+++ src/main/java/frc/robot/subsystems/roller/Roller.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.roller;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Roller extends SubsystemBase{
+    
+    //private final RollerIO m_io;
+   // private final RollerIOInputs m_inputs = new RollerIOInputs();
+
+    
+
+
+}
